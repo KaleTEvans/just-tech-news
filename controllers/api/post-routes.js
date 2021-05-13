4112ec4964039b7dc2f660b5b2034b62d0707ca7
KaleTEvans <kaletevans@gmail.com>
2021-05-13 14:16:52 -0500

    added comment and date helper functions

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -92,10 +92,14 @@ router.post('/', (req, res) => {
 
 // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
+  // make sure the session exists first
+  if (req.session) {
+    // pass session id 
     // custon static method
-    Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
+    Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+    .then(updatedVotetData => res.json(updatedVotetData))
     .catch(err => res.json(err));
+  }
 });
 
 router.put('/:id', (req, res) => {
